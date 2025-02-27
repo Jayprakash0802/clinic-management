3fd955939095403193e85f6376b9bdfe4a8cf52a
Jay Prakash <88879812+Jayprakash0802@users.noreply.github.com>
2025-02-28 02:44:46 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -14,7 +14,10 @@ const PORT = process.env.PORT || 5000;
 
 connectDB();
 
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  credentials: true
+}));
 app.use(express.json());
 
 app.use('/clinics', clinicRoutes);
